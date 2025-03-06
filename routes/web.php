9183a19c8b0ff0d96e1839c69b79dad5506666dc
Miguel Afanador <miguelafanquin10@gmail.com>
2025-03-06 11:06:46 -0500

    Primer Controllador Corregido

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,6 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 
 Route::get('/', HomeController::class);
-
 Route::get('/products', [ProductController::class, 'index']);
-
 Route::get('/products/create', [ProductController::class, 'create']);
-
 Route::get('/products/{name}', [ProductController::class, 'show']);
-
