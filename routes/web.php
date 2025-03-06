0e72a5cdf4799aa4ad74e1747c23c6bec3024e18
Miguel Afanador <miguelafanquin10@gmail.com>
2025-03-06 04:21:35 -0500

    rutas basicas

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/products', function () {
+    echo'LISTADO DE TODOS LOS PRODUCTOS';
+});
+
+Route::get('/products/create', function ($name) {
+    echo'FORMULARIO PARA CREAR UN PRODUCTO';
+});
+
+Route::get('/products/{name}/{category}', function ($name,$category) {
+    echo'PRODUCTO: $name, y tiene una categoria $category';
+});
+
