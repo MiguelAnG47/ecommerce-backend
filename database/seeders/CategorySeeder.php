04da63ce967de8add0bb60071311980b3d1af510
MiguelAnG47 <miguelafanquin10@gmail.com>
2025-03-27 08:59:24 -0500

    Creacion de Data Seeder

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $categoria_elec=new Category();
+        $categoria_elec->name='Electrodomestico';
+        $categoria_elec->description='Esta es la categoria del electrodomestico...';
+        $categoria_elec->save();
+
+
+        $categoria_2=new Category();
+        $categoria_2->name='Ropa';
+        $categoria_2->description='Esta es la categoria de Ropa...';
+        $categoria_2->save();
+
+    }
+}
